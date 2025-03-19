1643163d138ef9cb318ed23b3f7a162b40982d01
Giovanna Revito <giovannarev.roz@gmail.com>
2025-03-19 18:13:55 -0300

    ultimoultimo

--- checkpoint01/src/main/java/br/com/fiap/cp01/checkpoint01/exception/GlobalExceptionHandler.java
+++ checkpoint01/src/main/java/br/com/fiap/cp01/checkpoint01/exception/GlobalExceptionHandler.java
@@ -2,13 +2,16 @@ package br.com.fiap.cp01.checkpoint01.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.Map;
 
-
+@ControllerAdvice
 public class GlobalExceptionHandler {
+    @ExceptionHandler(LivroNaoEncontradoException.class)
     public ResponseEntity<Object> handlerLivroNaoEncontradoException (LivroNaoEncontradoException ex) {
         Map<String, Object> body = new HashMap<>();
 
