baf38042c1007e5d008de6bdd1375fdda2a4acef
Laboratório FIAP <labsfiap@fiap.com.br>
2025-03-21 10:40:49 -0300

    commit8

---
+++ checkpoint01/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into livro (titulo, autor, ano_publicacao, isbn, disponivel) values
+('O Senhor dos Anéis', 'J.R.R. Tolkien', 1954, '1234567890123', true),
+('1984', 'George Orwell', 1949, '9876543210123', true);
